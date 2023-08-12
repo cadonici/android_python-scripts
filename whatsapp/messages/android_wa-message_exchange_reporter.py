fa600469b81d1151d65e6fcb187b987cf879da7d
Luca Cadonici <luca.cadonici83@gmail.com>
2023-08-12 16:54:42 +0200

    WhatsApp Message Exchange Reporte
    This script, named "WhatsApp Message Exchange Reporter," retrieves and generates a report of all phone numbers involved in WhatsApp message exchanges. It connects to the respective databases, 'msgstore.db' and 'wa.db', and retrieves message counts and display names. The results are sorted by the message count in descending order and then by display name.
    
    Script Features:
    - Connects to 'msgstore.db' and 'wa.db' databases
    - Retrieves phone numbers and message counts
    - Retrieves display names from 'wa_contacts' in 'wa.db'
    - Sorts results by message count and display name
    
    Usage:
    1. Ensure the paths to 'msgstore.db' and 'wa.db' are correctly set.
    2. Run the script to generate the report.

---
+++ whatsapp/messages/android_wa-message_exchange_reporter.py
@@ -0,0 +1,68 @@
+import sqlite3
+# Explanation about the sorting criteria
+print("\nScript to generate a report of all phone numbers involved in WhatsApp message exchanges.\n \nResults are sorted by Message Count (descending) and then by Display Name. \n \n")
+
+# 
+
+# Connect to the msgstore.db database
+conn_msgstore = sqlite3.connect('msgstore.db')
+cursor_msgstore = conn_msgstore.cursor()
+
+# Query to retrieve phone numbers and their associated message counts
+query_msgstore = """
+    SELECT jid.user AS "Phone Number",
+           COUNT(*) AS "Message Count",
+           jid.raw_string AS "Raw String"
+    FROM jid
+    INNER JOIN chat ON chat.jid_row_id = jid._id
+    INNER JOIN message ON chat._id = message.chat_row_id
+    GROUP BY jid.user, jid.raw_string
+    ORDER BY "Message Count" DESC, jid.user;
+"""
+
+# Execute the query in msgstore.db
+cursor_msgstore.execute(query_msgstore)
+results_msgstore = cursor_msgstore.fetchall()
+
+# Close the connection to msgstore.db
+conn_msgstore.close()
+
+# Connect to the wa.db database
+conn_wa = sqlite3.connect('wa.db')
+cursor_wa = conn_wa.cursor()
+
+# Query to retrieve display names from wa_contacts in wa.db
+query_wa = """
+    SELECT jid, display_name
+    FROM wa_contacts
+    ORDER BY display_name;
+"""
+
+# Execute the query in wa.db
+cursor_wa.execute(query_wa)
+results_wa = cursor_wa.fetchall()
+
+# Close the connection to wa.db
+conn_wa.close()
+
+# Compare and print results
+sorted_results = []
+for msgstore_result in results_msgstore:
+    phone_number, message_count, raw_string = msgstore_result
+    for wa_result in results_wa:
+        wa_jid, display_name = wa_result
+        if raw_string == wa_jid:
+            sorted_results.append((display_name, phone_number, message_count, raw_string))
+            break
+
+# Sort results by Message Count (descending) and then by Display Name
+sorted_results.sort(key=lambda x: (x[2], x[0] if x[0] is not None else "" ), reverse=True)
+
+# Print sorted results
+for display_name, phone_number, message_count, raw_string in sorted_results:
+    print("Display Name:", display_name)
+    print("Phone Number:", phone_number)
+    print("Message Count:", message_count)
+    print("Raw String:", raw_string)
+    print("-" * 30)
+
