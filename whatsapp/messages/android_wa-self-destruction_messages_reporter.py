4ba0e81a74dbb8b8210e5414bb131ae95647cbb2
Luca Cadonici <luca.cadonici83@gmail.com>
2023-08-17 20:34:23 +0200

    Update android_wa-self-destruction_messages_reporter.py

--- whatsapp/messages/android_wa-self-destruction_messages_reporter.py
+++ whatsapp/messages/android_wa-self-destruction_messages_reporter.py
@@ -1,73 +1,39 @@
-print("**Android WhatsApp Self-destruction Reporter**\n")
-print("Developed by Luca Cadonici\n")
-print("This script is designed to retrieve and display information about WhatsApp contacts for whom the self-destruction setting is activated.")
-print("It connects to the 'msgstore.db' and 'wa.db' databases, presenting details on contacts with enabled self-destructing messages settings.\n")
-input("Press Enter to continue...")
-
-
 import sqlite3
+import os
 
-def main():
-    # Connect to the msgstore.db database
-    conn_msgstore = sqlite3.connect('msgstore.db')
-    cursor_msgstore = conn_msgstore.cursor()
+# Function to process the database
+def process_database(database_path, results_wa):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
 
-    # Define the SQL query for msgstore.db
     query_msgstore = '''
- SELECT
-        jid.raw_string,
-        jid.user AS "Phone Number",
-        CASE
-            WHEN message_ephemeral_setting.setting_duration = 0 THEN '(deactivated)'
-            WHEN message_ephemeral_setting.setting_duration > 86400 THEN
-                CAST(message_ephemeral_setting.setting_duration / 86400 AS VARCHAR) || ' days'
-            WHEN message_ephemeral_setting.setting_duration >= 3600 THEN
-                CAST(message_ephemeral_setting.setting_duration / 3600 AS VARCHAR) || ' hours'
-            WHEN message_ephemeral_setting.setting_duration >= 60 THEN
-                CAST(message_ephemeral_setting.setting_duration / 60 AS VARCHAR) || ' minutes'
-            ELSE
-                CAST(message_ephemeral_setting.setting_duration AS VARCHAR) || ' seconds'
-        END AS "Expiration Setting"
-    FROM
-        message
-    INNER JOIN
-        chat ON message.chat_row_id = chat._id
-    INNER JOIN
-        message_ephemeral_setting ON message._id = message_ephemeral_setting.message_row_id
-    INNER JOIN
-        jid ON chat.jid_row_id = jid._id
+        SELECT
+            jid.raw_string,
+            jid.user AS "Phone Number",
+            CASE
+                WHEN message_ephemeral_setting.setting_duration = 0 THEN '(deactivated)'
+                WHEN message_ephemeral_setting.setting_duration > 86400 THEN
+                    CAST(message_ephemeral_setting.setting_duration / 86400 AS VARCHAR) || ' days'
+                WHEN message_ephemeral_setting.setting_duration >= 3600 THEN
+                    CAST(message_ephemeral_setting.setting_duration / 3600 AS VARCHAR) || ' hours'
+                WHEN message_ephemeral_setting.setting_duration >= 60 THEN
+                    CAST(message_ephemeral_setting.setting_duration / 60 AS VARCHAR) || ' minutes'
+                ELSE
+                    CAST(message_ephemeral_setting.setting_duration AS VARCHAR) || ' seconds'
+            END AS "Expiration Setting"
+        FROM
+            message
+        INNER JOIN
+            chat ON message.chat_row_id = chat._id
+        INNER JOIN
+            message_ephemeral_setting ON message._id = message_ephemeral_setting.message_row_id
+        INNER JOIN
+            jid ON chat.jid_row_id = jid._id
     '''
-    
-    # Execute the query on msgstore.db
-    cursor_msgstore.execute(query_msgstore)
-    results_msgstore = cursor_msgstore.fetchall()
 
-    # Close the connection to msgstore.db
-    conn_msgstore.close()
-
-    # Connect to the wa.db database
-    conn_wa = sqlite3.connect('wa.db')
-    cursor_wa = conn_wa.cursor()
-
-    # Query to retrieve display names and WhatsApp names from wa_contacts in wa.db
-    query_wa = """
-        SELECT jid, display_name, wa_name
-        FROM wa_contacts
-        ORDER BY display_name;
-    """
-
-    # Execute the query in wa.db
-    cursor_wa.execute(query_wa)
-    results_wa = cursor_wa.fetchall()
-
-    # Close the connection to wa.db
-    conn_wa.close()
-
-    # Compare and print results
-
- 
-
-
+    cursor.execute(query_msgstore)
+    results_msgstore = cursor.fetchall()
+    conn.close()
 
     sorted_results = {}
     for msgstore_result in results_msgstore:
@@ -84,93 +50,84 @@ def main():
                 else:
                     sorted_results[key].append(expiration_setting)
                 break
-    
-    # Connect to the wa.db database
+
+    return sorted_results
+
+def main():
+    print("**Android WhatsApp Self-destruction Reporter**\n")
+    print("Developed by Luca Cadonici\n")
+    print("This script connects to the 'msgstore.db' database, retrieving and displaying details on contacts with enabled self-destructing message settings.")
+    print("It also connects to the 'wa.db' database to retrieve display names and WhatsApp names of contacts. If decrypted backup files of 'msgstore.db' are present in the folder, they will be analyzed.")
+    input("Press Enter to continue...")
+    print("Please ensure you have the 'msgstore.db', 'wa.db' databases, and any decrypted backup .db files (NO .crypt12, .crypt13, .crypt14 etc.) in the same folder.")
+    input("Press Enter to continue...")
     conn_wa = sqlite3.connect('wa.db')
     cursor_wa = conn_wa.cursor()
 
-    # Define the SQL query for wa.db (contacts with self-destructing messages)
-    query_wa_self_destruct = '''
-    SELECT
-        number AS "Phone Number",
-        display_name AS "Display Name",
-        wa_name AS "WhatsApp Name",
-        CASE
-            WHEN disappearing_mode_duration >= 86400 THEN
-                CAST(disappearing_mode_duration / 86400 AS VARCHAR) || ' days'
-            WHEN disappearing_mode_duration >= 3600 THEN
-                CAST(disappearing_mode_duration / 3600 AS VARCHAR) || ' hours'
-            WHEN disappearing_mode_duration >= 60 THEN
-                CAST(disappearing_mode_duration / 60 AS VARCHAR) || ' minutes'
-            ELSE
-                CAST(disappearing_mode_duration AS VARCHAR) || ' seconds'
-        END AS "Expiration Setting"
-    FROM 
-        wa_contacts
-    WHERE disappearing_mode_duration IS NOT null
-    '''
-    
-    # Execute the query on wa.db
-    cursor_wa.execute(query_wa_self_destruct)
-    result_wa = cursor_wa.fetchall()
+    query_wa = """
+        SELECT jid, display_name, wa_name
+        FROM wa_contacts
+        ORDER BY display_name;
+    """
 
-
-
+    cursor_wa.execute(query_wa)
+    results_wa = cursor_wa.fetchall()
     conn_wa.close()
 
-    # Calculate total contacts with self-destruction
-    total_msgstore_results = len(sorted_results)
-    total_wa_results = len(result_wa)
-    total_contacts_with_self_destruction = total_msgstore_results + total_wa_results
+    # Process 'msgstore.db'
+    print("Processing data from: msgstore.db")
+    msgstore_db_results = process_database("msgstore.db", results_wa)
 
-    print("\nTotal contacts with self-destruction from 'msgstore.db':", total_msgstore_results)
-    print("Total contacts with self-destruction from 'wa.db':", total_wa_results)
-    print("Overall total of contacts with self-destruction:", total_contacts_with_self_destruction)
-
-
-    input("\nPress Enter to retrieve results from 'msgstore.db'...")
-
-
-
-    
-    # Print the consolidated results
+    # Display results from 'msgstore.db'
+    total_msgstore_results = len(msgstore_db_results)
+    print("\nTotal contacts with self-destruction in msgstore.db:", total_msgstore_results)
     print("\nConsolidated results from 'msgstore.db':")
-    for key, expiration_settings in sorted_results.items():
-        display_name, wa_name, phone_number = key
-        print("Display Name:", display_name)
-        print("WhatsApp Name:", wa_name)
-        print("Phone Number:", phone_number)
-        
-        # Replace "0 seconds" with "deactivated" in expiration settings
-        adjusted_settings = [setting if setting != "0 seconds" else "(deactivated)" for setting in expiration_settings]
-        adjusted_settings_string = ", ".join(adjusted_settings)
-        
-        # Remove the comma before "deactivated"
-        adjusted_settings_string = adjusted_settings_string.replace(", (deactivated)", " (deactivated)")
-        
-        print("Expiration Settings:", adjusted_settings_string)
+    for key, expiration_setting in msgstore_db_results.items():
+        print("Display Name:", key[0])
+        print("WhatsApp Name:", key[1])
+        print("Phone Number:", key[2])
+        print("Expiration Setting:", expiration_setting)
         print("-" * 30)
 
+    # Prompt to extend the operation to other backup files
+    prompt = input("\nDo you want to extend the operation to other backup files? (yes/no): ").strip().lower()
+    if prompt == "yes":
+        # Get a list of backup files with 'msgstore' in the name
+        database_files = [filename for filename in os.listdir('.') if "msgstore" in filename and filename != "msgstore.db"]
 
-    
-    # Remove the comma before "deactivated"
-    adjusted_settings_string = adjusted_settings_string.replace(", deactivated", " deactivated")
-    
-
-
-
-    input("\nPress Enter to retrieve results from 'wa.db'...")
-    print("\nResults from 'wa.db':\n")
-    for contact_row in result_wa:
-        phone_number = contact_row[0]
-        display_name = contact_row[1]
-        wa_name = contact_row[2]
-        expiration_setting = contact_row[3]
-        print("Phone Number:", phone_number)
-        print("Display Name:", display_name)
-        print("WhatsApp Name:", wa_name)
-        print("Expiration Setting:", expiration_setting)
-        print("------------------------")
+        for db_file in database_files:
+            if db_file != "msgstore.db":
+                print("\nProcessing data from:", db_file)
+                backup_results = process_database(db_file, results_wa)
+                total_backup_results = len(backup_results)
+                print(f"\nTotal contacts with self-destruction in {db_file}: {total_backup_results}")
+                
+                differences_found = False
+                for key, expiration_setting in msgstore_db_results.items():
+                    if key not in backup_results:
+                        differences_found = True
+                        break
+                
+                if differences_found:
+                    print(f"\nDifferences from 'msgstore.db' analysis:")
+                    for key, expiration_setting in msgstore_db_results.items():
+                        if key not in backup_results:
+                            print(f"File: {db_file}\n")
+                            print("Display Name:", key[0])
+                            print("WhatsApp Name:", key[1])
+                            print("Phone Number:", key[2])  # Extract the phone number from the key tuple
+                            print("Expiration Setting:", "Not Activated")
+                            print("-" * 30)
+                    for key, expiration_setting in backup_results.items():
+                        if key not in msgstore_db_results:
+                            print(f"File: {db_file}\n")
+                            print("Display Name:", key[0])
+                            print("WhatsApp Name:", key[1])
+                            print("Phone Number:", key[2])  # Extract the phone number from the key tuple
+                            print("Expiration Setting:", expiration_setting)
+                            print("-" * 30)
+                else:
+                    print("No differences from msgstore.db")
 
 if __name__ == '__main__':
     main()
